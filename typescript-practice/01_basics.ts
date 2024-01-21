5646bf8876bb35753e4691ebc4a3f3788d3208d3
Yiqun Huang <albert913388672@gmail.com>
2024-01-20 23:58:39 -0800

    finish the 01basics

--- typescript-practice/01_basics.ts
+++ typescript-practice/01_basics.ts
@@ -1,4 +1,12 @@
-let myNum: number | string= 5;
+let myNum: number | string | boolean = 5;
 
 myNum = 42;
+myNum = 'Hello';
+myNum = true; // Error
 
+const nums: (number | boolean)[] = [];
+nums.push(5);
+nums.push(true);
+// nums.push('Hello'); // Error
+
+const myVar = nums.pop();
